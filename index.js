cd0e02cc2606ffef653e2f120b4be9928e7f0ffc
Kaemerle <carlkaemerle@yahoo.com>
2022-03-31 11:35:55 -0700

    i cant get the input of playerSelection() to be case insensitive its driving me insane

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
-let randomNumber = Math.floor(Math.random() * 3) + 1;
+let options = ["rock", "paper", "scissors"];
+let choice = options[Math.floor(Math.random() * options.length)];
+const computerSelection = computerPlay();
+
+
+
 
 function computerPlay() {
-    if (randomNumber === 1) {
-        console.log("Rock");
-    } else if (randomNumber === 2) {
-        console.log("Paper");
-    } else {
-        console.log("Scissors");
+    console.log(choice)
+    return choice;
+}
+
+function playerSelection(pick) {
+    if (pick.toLowerCase() === 'rock'.toLowerCase()) {
+        return 'rock';
+    } else if(pick.toLowerCase() === 'paper'.toLowerCase()) {
+        return 'paper';
+    } else if(pick.toLowerCase() === 'scissors'.toLowerCase()) {
+        return 'scissors';
+    }  else {
+        return 'Player 1: Please select rock, paper, or scissors.'
     }
 }
 
-computerPlay()
-console.log(randomNumber)
+function playRound(playerSelection, computerPlay) {
+    if ((playerSelection === 'rock') && (computerPlay === 'scissors')) {
+        console.log('You win, rock beats scissors!');
+        return "Winner";
+    } else if ((playerSelection === 'rock') && (computerPlay === 'paper')) {
+        console.log('You lose, paper beats rock!');
+        return "Loser";
+    } else if ((playerSelection === 'rock') && (computerPlay === 'rock')) {
+        console.log('Its a tie, play another round!');
+        return "Tie";
+    } else if ((playerSelection === 'paper') && (computerPlay === 'scissors')) {
+        console.log('You lose, scissors beats paper!');
+        return "Loser";
+    } else if ((playerSelection === 'paper') && (computerPlay === 'paper')) {
+        console.log('Its a tie, play another round');
+        return "Tie";
+    } else if ((playerSelection === 'paper') && (computerPlay === 'rock')) {
+        console.log('You win, paper beats rock!');
+        return "Winner";
+    } else if ((playerSelection === 'scissors') && (computerPlay === 'rock')) {
+        console.log('You lose, rock beats scissors!');
+        return "Loser";
+    } else if ((playerSelection === 'scissors') && (computerPlay === 'scissors')) {
+        console.log('Its a tie, play another round!');
+        return "Tie";
+    } else if ((playerSelection === 'scissors') && (computerPlay === 'paper')) {
+        console.log('You win, scissors beats scissors!');
+        return "Winner";
+    } else {
+        console.log('Something went very wrong');
+    }
+}
+
+playerSelection = 'rock'
+console.log(playRound(playerSelection, computerSelection));
